97a14f6b1d75a852eca6526da60e5735a0a715c9
fangyang <407455574@qq.com>
2018-07-30 13:36:14 +0800

    冲突TEST

--- src/com/test/Test1.java
+++ src/com/test/Test1.java
@@ -8,14 +8,14 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		/*
-		 * 输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数
+		 * 杈撳叆涓�琛屽瓧绗︼紝鍒嗗埆缁熻鍑哄叾涓嫳鏂囧瓧姣嶃�佺┖鏍笺�佹暟瀛楀拰鍏跺畠瀛楃鐨勪釜鏁�
 		 * 
 		 * */
 		BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
 		int len = 0, ben=0, num = 0, space = 0, others = 0,ch=0, sum=0;
 		String a = "";
         byte[]str = null;
-        System.out.println("请输入A:");
+        System.out.println("璇疯緭鍏:");
         try {
 			a = s.readLine();
 		} catch (IOException e) {
@@ -45,15 +45,15 @@ public class Test1 {
 			}
 		}
 		sum=len+ben+num+space+ch/2+others;
-		System.out.println("小写字母个数:"+len);
-		System.out.println("大写字母个数:"+ben);
-		System.out.println("数字个数:"+num);
-		System.out.println("空格个数:"+space);
-		System.out.println("汉字个数:"+ch/2);
-		System.out.println("字符个数:"+others);
-		System.out.println("总个数:"+sum);
+		System.out.println("灏忓啓瀛楁瘝涓暟:"+len);
+		System.out.println("澶у啓瀛楁瘝涓暟:"+ben);
+		System.out.println("鏁板瓧涓暟:"+num);
+		System.out.println("绌烘牸涓暟:"+space);
+		System.out.println("姹夊瓧涓暟:"+ch/2);
+		System.out.println("瀛楃涓暟:"+others);
+		System.out.println("鎬讳釜鏁�:"+sum);
 		System.out.println("123456");
-		System.out.println("456123123");
+		System.out.println("45612");
 		System.out.println("456123123");
 		System.out.println("456123123");
 	}	
